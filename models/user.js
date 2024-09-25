8ba329905f6111e46793027f8d23c54838328060
Astrid Ge <qiutongg@umich.edu>
2024-09-25 16:10:17 -0700

    complete data schema

--- models/user.js
+++ models/user.js
@@ -9,8 +9,31 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationSchema], //! embedding the applicationSchema here
 });
 
-const User = mongoose.model('User', userSchema);
 
+const applicationSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],
+  },
+});
+
+
+const User = mongoose.model('User', userSchema);
 module.exports = User;
